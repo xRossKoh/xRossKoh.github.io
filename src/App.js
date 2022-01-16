74152b4e5d7ac37104ba30a28c2edc5561117de8
xRossKoh <e0543512@u.nus.edu>
2022-01-16 21:31:09 +0800

    Created personal website

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my website!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          I don't really know what I am doing here
+        </p>
+        <button className='btn btn-secondary'>
+          Click here!
+        </button>
       </header>
     </div>
   );
